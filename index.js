5f2b00058dcf9655423e4f114c9f98b0608d992a
Nguyen Hien <s4020756@rmit.edu.vn>
2025-11-18 13:22:28 +0700

    create folder structure

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`listening on http://localhost:${port}`)
 })

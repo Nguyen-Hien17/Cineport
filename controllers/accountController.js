428b65634505e034567a16b80ab91e892ff9b3e2
Nguyen Hien <s4020756@rmit.edu.vn>
2025-12-21 10:14:20 +0700

    minor bug fix

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -3,7 +3,7 @@ const accountService = require('../services/accountService');
 exports.index = async (req, res) => {
     let userId = req.session.userId;
 
-    let user = accountService.getAccountDetails(userId);
+    let user = await accountService.getAccountDetails(userId);
 
     res.render('account', { user: user });
 }

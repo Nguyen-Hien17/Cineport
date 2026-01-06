0843af13b00e8826f2ddac590d864c712730e8d0
Nguyen Hien <s4020756@rmit.edu.vn>
2026-01-06 11:49:22 +0700

    fix region feature

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,6 +15,12 @@ exports.login = async (req, res) => {
         const userObj = user.toObject();
         delete userObj.password;
         req.session.user = userObj;
+        req.session.region =
+            user.region ||
+            req.body.region ||
+            req.query.region ||
+            'US';
+        console.log('User logged in with region:', req.session.region);
         res.redirect('/');
     } else {
         return res.status(401).send({ message: 'Invalid credentials.' });
